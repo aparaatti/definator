5e4507bb69fe81efaad3fd19770900789bc7f2e2
Niko Humalamäki <nikohuma@gmail.com>
2014-08-14 12:43:26 +0300

    qtdesigner generated ui py file. Raises event when term is selected.

---
+++ src/qtdesigner/ui_QTermList.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'QTermList.ui'
+#
+# Created: Thu Aug 14 11:56:46 2014
+#      by: PyQt5 UI code generator 5.3.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_QTermList(object):
+    def setupUi(self, QTermList):
+        QTermList.setObjectName("QTermList")
+        QTermList.resize(166, 210)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(QTermList.sizePolicy().hasHeightForWidth())
+        QTermList.setSizePolicy(sizePolicy)
+        QTermList.setMinimumSize(QtCore.QSize(166, 200))
+        QTermList.setMaximumSize(QtCore.QSize(166, 16777215))
+        self.verticalLayout = QtWidgets.QVBoxLayout(QTermList)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.lineEdit = QtWidgets.QLineEdit(QTermList)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
+        self.lineEdit.setSizePolicy(sizePolicy)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.widget = QtWidgets.QWidget(QTermList)
+        self.widget.setObjectName("widget")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.widget)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.listWidget = QtWidgets.QListWidget(self.widget)
+        self.listWidget.setObjectName("listWidget")
+        self.verticalLayout_2.addWidget(self.listWidget)
+        self.verticalLayout.addWidget(self.widget)
+        self.buttonAddTerm = QtWidgets.QPushButton(QTermList)
+        self.buttonAddTerm.setObjectName("buttonAddTerm")
+        self.verticalLayout.addWidget(self.buttonAddTerm)
+
+        self.retranslateUi(QTermList)
+        QtCore.QMetaObject.connectSlotsByName(QTermList)
+
+    def retranslateUi(self, QTermList):
+        _translate = QtCore.QCoreApplication.translate
+        QTermList.setWindowTitle(_translate("QTermList", "Term list"))
+        self.lineEdit.setToolTip(_translate("QTermList", "<html><head/><body><p>Filter out terms by writing here</p></body></html>"))
+        self.lineEdit.setText(_translate("QTermList", "Filter"))
+        self.buttonAddTerm.setText(_translate("QTermList", "&Add term"))
+
--- src/term_list.py
+++ src/term_list.py
@@ -1,36 +1,39 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QStandardItemModel
+from PyQt5.QtWidgets import QWidget, QMessageBox
 
 from qtdesigner.ui_QTermList import *
 
+
 class TermList(QWidget, Ui_QTermList):
-    
     def __init__(self, parent=None):
         #Load QWidget
         super(TermList, self).__init__(parent)
 
         #Setup widgets from Ui_QTermList (qtdesigner made ui):
         self.setupUi(self)
-        self.register_add_term_listener(self.addTerm)
 
         #Data:
         self.populate_list_view(("New", "Old"))
 
     def populate_list_view(self, str_tuple: tuple):
-        """
-        :type self: TermList
-        """
         for row, name in enumerate(str_tuple):
-            self.listWidget.insertItem(row,name)
+            self.listWidget.insertItem(row, name)
 
-    def register_add_term_listener(self, methodName):
-        self.buttonAddTerm.clicked.connect(methodName)
+    def register_add_term_listener(self, method):
+        self.buttonAddTerm.clicked.connect(method)
 
-    def addTerm(self):
-        self.listWidget.insertItem(0,"Kissa")
+    def register_term_selected_listener(self, method):
+        self.listWidget.itemDoubleClicked.connect(method)
+
+    def selected(self):
+        item = self.sender()
+        #print(dir(self.sender()))
+        #print(item.currentItem().text())
+        QMessageBox.information(self,item.currentItem().text(), item.currentItem().text())
+
 
 if __name__ == "__main__":
     import sys
+    from PyQt5.QtWidgets import QApplication
     app = QApplication(sys.argv)
     form = TermList()
     form.show()
