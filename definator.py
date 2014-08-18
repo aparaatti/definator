f65ef48dd25034ada417423ae7e3ca4ef41c6b11
Niko Humalamäki <nikohuma@gmail.com>
2014-08-18 09:56:48 +0300

    version 0.001, before refactoring modules

--- definator.py
+++ definator.py
@@ -2,7 +2,7 @@
 # and it is licensed under the GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt).
 #
 __author__ = "Niko Humalamäki"
-__version__ = "0.0.1"
+__version__ = "0.001"
 
 import sys
 import src.qdefinator_gui
