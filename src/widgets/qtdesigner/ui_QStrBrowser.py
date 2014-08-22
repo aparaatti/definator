60c3068b254933e13530fade90967b71db2c5008
Niko Humalamäki <nikohuma@gmail.com>
2014-08-22 10:25:55 +0300

    QTermList --> QStrBrowser

--- src/widgets/qtdesigner/ui_QStrBrowser.py
+++ src/widgets/qtdesigner/ui_QStrBrowser.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'QTermList.ui'
+# Form implementation generated from reading ui file 'QStrBrowser.ui'
 #
-# Created: Fri Aug 15 21:57:00 2014
+# Created: Wed Aug 20 12:04:22 2014
 #      by: PyQt5 UI code generator 5.3.1
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_QTermList(object):
-    def setupUi(self, QTermList):
-        QTermList.setObjectName("QTermList")
-        QTermList.resize(166, 210)
+class Ui_QStrBrowser(object):
+    def setupUi(self, QStrBrowser):
+        QStrBrowser.setObjectName("QStrBrowser")
+        QStrBrowser.resize(166, 210)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(QTermList.sizePolicy().hasHeightForWidth())
-        QTermList.setSizePolicy(sizePolicy)
-        QTermList.setMinimumSize(QtCore.QSize(166, 200))
-        QTermList.setMaximumSize(QtCore.QSize(166, 16777215))
-        self.verticalLayout = QtWidgets.QVBoxLayout(QTermList)
+        sizePolicy.setHeightForWidth(QStrBrowser.sizePolicy().hasHeightForWidth())
+        QStrBrowser.setSizePolicy(sizePolicy)
+        QStrBrowser.setMinimumSize(QtCore.QSize(166, 200))
+        QStrBrowser.setMaximumSize(QtCore.QSize(166, 16777215))
+        self.verticalLayout = QtWidgets.QVBoxLayout(QStrBrowser)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.lineEdit = QtWidgets.QLineEdit(QTermList)
+        self.lineEdit = QtWidgets.QLineEdit(QStrBrowser)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -30,7 +30,7 @@ class Ui_QTermList(object):
         self.lineEdit.setSizePolicy(sizePolicy)
         self.lineEdit.setObjectName("lineEdit")
         self.verticalLayout.addWidget(self.lineEdit)
-        self.widget = QtWidgets.QWidget(QTermList)
+        self.widget = QtWidgets.QWidget(QStrBrowser)
         self.widget.setObjectName("widget")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.widget)
         self.verticalLayout_2.setSpacing(0)
@@ -40,17 +40,13 @@ class Ui_QTermList(object):
         self.listWidget.setObjectName("listWidget")
         self.verticalLayout_2.addWidget(self.listWidget)
         self.verticalLayout.addWidget(self.widget)
-        self.buttonAddTerm = QtWidgets.QPushButton(QTermList)
-        self.buttonAddTerm.setObjectName("buttonAddTerm")
-        self.verticalLayout.addWidget(self.buttonAddTerm)
 
-        self.retranslateUi(QTermList)
-        QtCore.QMetaObject.connectSlotsByName(QTermList)
+        self.retranslateUi(QStrBrowser)
+        QtCore.QMetaObject.connectSlotsByName(QStrBrowser)
 
-    def retranslateUi(self, QTermList):
+    def retranslateUi(self, QStrBrowser):
         _translate = QtCore.QCoreApplication.translate
-        QTermList.setWindowTitle(_translate("QTermList", "Term list"))
-        self.lineEdit.setToolTip(_translate("QTermList", "<html><head/><body><p>Filter out terms by writing here</p></body></html>"))
-        self.lineEdit.setText(_translate("QTermList", "Filter"))
-        self.buttonAddTerm.setText(_translate("QTermList", "&Add term"))
+        QStrBrowser.setWindowTitle(_translate("QStrBrowser", "Term list"))
+        self.lineEdit.setToolTip(_translate("QStrBrowser", "<html><head/><body><p>Filter out terms by writing here</p></body></html>"))
+        self.lineEdit.setText(_translate("QStrBrowser", "Filter"))
 
--- src/widgets/qtdesigner/ui_QTermDisplay.py
+++ src/widgets/qtdesigner/ui_QTermDisplay.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'QTermDisplay.ui'
 #
-# Created: Fri Aug 15 21:56:59 2014
+# Created: Wed Aug 20 12:04:23 2014
 #      by: PyQt5 UI code generator 5.3.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,37 +12,26 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_TermDisplay(object):
     def setupUi(self, TermDisplay):
         TermDisplay.setObjectName("TermDisplay")
-        TermDisplay.resize(548, 435)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        TermDisplay.resize(658, 429)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(TermDisplay.sizePolicy().hasHeightForWidth())
         TermDisplay.setSizePolicy(sizePolicy)
-        self.verticalLayout_4 = QtWidgets.QVBoxLayout(TermDisplay)
-        self.verticalLayout_4.setObjectName("verticalLayout_4")
-        self.scrollArea = QtWidgets.QScrollArea(TermDisplay)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setObjectName("scrollArea")
-        self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 528, 415))
-        self.scrollAreaWidgetContents.setMinimumSize(QtCore.QSize(400, 150))
-        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout = QtWidgets.QVBoxLayout(TermDisplay)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetMaximumSize)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.termDisplay = QtWebKitWidgets.QWebView(self.scrollAreaWidgetContents)
-        self.termDisplay.setEnabled(True)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        self.contentWebView = QtWebKitWidgets.QWebView(TermDisplay)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.termDisplay.sizePolicy().hasHeightForWidth())
-        self.termDisplay.setSizePolicy(sizePolicy)
-        self.termDisplay.setMinimumSize(QtCore.QSize(400, 400))
-        self.termDisplay.setAcceptDrops(False)
-        self.termDisplay.setUrl(QtCore.QUrl("file:///home/aparaatti/Code/Python/definator/TermBase/template-term.html"))
-        self.termDisplay.setObjectName("termDisplay")
-        self.verticalLayout.addWidget(self.termDisplay)
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.verticalLayout_4.addWidget(self.scrollArea)
+        sizePolicy.setHeightForWidth(self.contentWebView.sizePolicy().hasHeightForWidth())
+        self.contentWebView.setSizePolicy(sizePolicy)
+        self.contentWebView.setUrl(QtCore.QUrl("about:blank"))
+        self.contentWebView.setObjectName("contentWebView")
+        self.verticalLayout.addWidget(self.contentWebView)
 
         self.retranslateUi(TermDisplay)
         QtCore.QMetaObject.connectSlotsByName(TermDisplay)
