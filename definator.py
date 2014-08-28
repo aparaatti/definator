34b728af7c170ac17e9c81c1278f1375ddf46161
Niko Humalamäki <nikohuma@gmail.com>
2014-08-28 10:37:47 +0300

    List of things to do.

--- definator.py
+++ definator.py
@@ -1,6 +1,23 @@
 # This file is a part of Definator (https://github.com/aparaatti/definator)
 # and it is licensed under the GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt).
 #
+# "Road map":
+# TODO: saving of terms.json
+# TODO: creation of new project
+# TODO: linking of terms (dialog for linking, module to show linked terms=)
+# TODO: split description text to description objects
+#   TODO: handling of tags in text (Paragraph, Title)
+#   TODO: buttons/icons to add tags
+# TODO: linking of files
+# TODO: tagging images to text (ImagePath)
+#
+# TODO Later:
+#       str-browser as own widget to left side and MainWidget contains term display and editor.
+#       Maybe open terms in tabs, with editor/term --> proper undo/redo
+#       Save term (ctrl + s) + save project + save project as
+#       Writing unit tests
+#
+#
 from PyQt5.QtWidgets import QApplication
 import sys
 import src.qdefinator_gui
@@ -10,3 +27,4 @@ if __name__ == "__main__":
     form = src.qdefinator_gui.MainWindow()
     form.show()
     app.exec()
+
