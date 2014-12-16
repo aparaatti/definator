ca21ba4353da475ba1e090dc04ae2e5ac7957ff2
Niko Humalamäki <nikohuma@gmail.com>
2014-12-16 22:37:21 +0200

    Todo from definato.py to todo.md

--- definator.py
+++ definator.py
@@ -3,32 +3,6 @@
 #
 # This file is a part of Definator (https://github.com/aparaatti/definator)
 # and it is licensed under the GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt).
-#
-# "Road map":
-# TODO: All menu items
-#   -settings
-#      -language
-#       -remember opened projects
-#       -load last opened project on startup
-#       -colors of files in linked things
-# TODO Later:
-#   -Generic tag class, that can be used to define a tag and html
-#    representation for it.
-#   -term browser in side pane (QDockWidget).
-#   -File copy doesn't work on windows (?)
-#   -Maybe open terms in tabs, with editor/term --> proper undo/redo
-#   -Writing of unit tests
-#   -Add list of synonymous terms to a term.
-#   -show tags bold on term editor, show references to images bold on term
-#       display
-#   -maybe different types of links for terms (sub term, parent term)
-#   -Use python built in serialization/de serialization for saving stuff
-#   -Warn user when removing a term that also the linked files in term folder
-#   will be deleted.
-#
-# RE FACTOR:
-#   -TermDisplay to HtmlDisplay
-#
 import sys
 import os
 import logging
---
+++ todo.md
@@ -0,0 +1,24 @@
+##"Road map"
+# TODO: All menu items
+   -settings
+      -language
+       -remember opened projects
+       -load last opened project on startup
+       -colors of files in linked things
+# TODO Later:
+   -Generic tag class, that can be used to define a tag and html
+    representation for it.
+   -term browser in side pane (QDockWidget).
+   -File copy doesn't work on windows (?)
+   -Maybe open terms in tabs, with editor/term --> proper undo/redo
+   -Writing of unit tests
+   -Add list of synonymous terms to a term.
+   -show tags bold on term editor, show references to images bold on term
+       display
+   -maybe different types of links for terms (sub term, parent term)
+   -Use python built in serialization/de serialization for saving stuff
+   -Warn user when removing a term that also the linked files in term folder
+   will be deleted.
+
+# RE FACTOR:
+   -TermDisplay to HtmlDisplay
